57accd0bf436dcb5295c288d7969f27f86d7c329
KevinPena2022 <pen21006@byui.edu>
2022-05-10 21:56:35 -0700

    lastone as a team

--- game/game.py
+++ game/game.py
@@ -1,7 +1,5 @@
 import random
 
-from sqlalchemy import true
-
 class Game():
     def __init__(self):
      self.player_score = 300
@@ -14,41 +12,32 @@ class Game():
         user_guess = input('Higher or lower? [h/l]: ')
         card_number = random.randint(1,13)
         print('Next card was: ' + str(card_number))
-
         if user_guess == 'h' and card_number > card_number1:
             self.player_score += 100
-        else: 
-            self.player_score -= 75
-          
-                
+        elif user_guess == 'l' and card_number < card_number1:
+            self.player_score += 100
+        else: self.player_score -= 75
 
         print('Your score is: ' + str(self.player_score))
 
+            
 
-    # def printing_cards(card_number):
-    #     print('The next card was: ' + str(card_number))
-    
-    # def validation_user(self):
-    #     asking_user = 
-        
 
 
 def main():
-    playing = true
+    playing = True
     user_question = ''
 
     while playing:
         play1 = Game()
         play1.display_card()
         user_question = input('Play Again? [y/n] ')
-        print()
         if user_question == 'y':
-            play1.display_card()
-            user_question = input('Play Again? [y/n] ')
+            main()
         else:
-            playing == False
-            break
-
+            playing == False  
+        print()
+        
    
 
 
