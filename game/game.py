037b0e3974b429a8894c61f326c2a9f791722cd6
KevinPena2022 <pen21006@byui.edu>
2022-05-11 18:54:50 -0700

    Final Game

--- game/game.py
+++ game/game.py
@@ -1,27 +1,47 @@
 import random
 
+# Creating a Cards Class
+class Cards:
+    def __init__(self):
+        self.card1 = 0
+        self.card2 = 2
+        
+    def get_card_value(self):
+        self.card1 = random.randint(1,13)
+        self.card2 = random.randint(1,13)
+
+    def get_new_card(self):
+        self.card1 = self.card2
+        self.card2 = random.randint(1,13)
+
 # Creating a Game Class
 class Game():
     def __init__(self):
-     self.player_score = 300
+        self.player_score = 300
+        self.cards = Cards()
+        self.cards.get_card_value()
 
     # Method from the Game Class
     def display_card(self):
-        card_number1 = random.randint(1,13)
+        #card_number1 = self.cards.card1
+        #card_number2 = self.cards.card2
         print()
-        print("The card is: "+ str(card_number1))
+        print("The card is: "+ str(self.cards.card1))
         user_guess = input('Higher or lower? [h/l]: ')
-        card_number = random.randint(1,13)
-        print('Next card was: ' + str(card_number))
+        # card_number = random.randint(1,13)
+        print('Next card was: ' + str(self.cards.card2))
        
         # This is the Validation Section 
-        if user_guess == 'h' and card_number > card_number1:
+        if user_guess == 'h' and self.cards.card2 > self.cards.card1:
             self.player_score += 100
-        elif user_guess == 'l' and card_number < card_number1:
+        elif user_guess == 'l' and self.cards.card2 < self.cards.card1:
             self.player_score += 100
-        else: self.player_score -= 75
+        else: 
+            self.player_score -= 75
 
         print('Your score is: ' + str(self.player_score))
+
+        self.cards.get_new_card()
 
             
 
@@ -30,15 +50,16 @@ class Game():
 def main():
     playing = True
     user_question = ''
+    play1 = Game()
 
     while playing:
-        play1 = Game()
         play1.display_card()
         user_question = input('Play Again? [y/n] ')
         if user_question == 'y':
-            main()
+            #main() 
+            pass
         else:
-            playing == False  
+            playing = False
         print()
         
    
